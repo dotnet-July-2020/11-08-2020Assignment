a2d2ab500adc37f703b7d667027e506e44ffb127
Geethika <geethika.duddupudi@zensar.com>
2020-08-11 22:47:44 +0530

    Create Calculator class library

---
+++ ClassCalculator/ClassCalculator/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCalculator
+{
+    [Serializable]
+    public class Class1
+    {
+        public int Num1
+        {
+            get;
+            set;
+        }
+        public int Num2
+        {
+            get;
+            set;
+        }
+        public int Add()
+        {
+            return Num1 + Num2;
+        }
+        public int Sub()
+        {
+            return Num1 - Num2;
+
+        }
+        public int Mul()
+        {
+            return Num1 * Num2;
+        }
+        public int Div()
+        {
+            return Num1 / Num2;
+        }
+    }
+}
