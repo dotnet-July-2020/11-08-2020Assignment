c01207d78bc8b4bf8df277a7d864316ba708ea31
Geethika <geethika.duddupudi@zensar.com>
2020-08-11 22:50:59 +0530

    Create binary serialization for class calculator

---
+++ SerializationAssignment/SerializationAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using ClassCalculator;
+
+namespace SerializationAssignment
+{
+  [Serializable]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            FileStream fs = new FileStream("Calculator.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter sw = new BinaryFormatter();
+            sw.Serialize(fs, obj);
+            Console.WriteLine("Text File Created");
+            Console.Read();
+        }
+    }
+}
